03cc1fd26a72c8fb3ae671dc6a6f89d7dd20d71b
nirsopher <nirsopher@gmail.com>
2023-05-09 00:08:57 +0300

    config from yaml

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 mkdir -p  out
 g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) -g brain_pybind/brain.cpp -g src/*/*.cpp -I include -std=c++17 -o out/brain$(python3-config --extension-suffix)
-cp py/*.py out
+cp py/brain out/brain-py
