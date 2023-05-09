257a6f654fc2bf05e709051b5b30dac041054206
nirsopher <nirsopher@gmail.com>
2023-05-09 10:16:57 +0300

    readme

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-mkdir -p  out
-g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) -g brain_pybind/brain.cpp -g src/*/*.cpp -I include -std=c++17 -o out/brain$(python3-config --extension-suffix)
-cp py/brain out/brain-py
+mkdir -p $1
+g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) -g brain_pybind/brain.cpp -g src/*/*.cpp -I include -std=c++17 -o $1/brain$(python3-config --extension-suffix)
+cp py/brain $1/brain
